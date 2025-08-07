9ccc293e5226cfb2387bb32f007ba8eee7d9c829
Breno Andrade <Breno.Araujo@consulting-for.edenred.com>
2025-08-07 12:05:31 -0300

    Set base name url and handle 404

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/sync-it-beat-flow">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/auth" element={<Auth />} />
